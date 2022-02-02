ecc7edc60a0248eab2071fd621127de19fef7aeb
riko-teki <boiledshrimps0109@gmail.com>
2022-02-02 15:38:43 +0900

    step 6

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use libc::STDIN_FILENO;
+use libc::{STDIN_FILENO, TCSAFLUSH};
 use nix::unistd;
 use termios::*;
 
 fn main() {
-    enable_raw_mode();
+    let raw_terminal = RawTerminal::enable_raw_mode();
     let mut c: [u8;1] = [0];
     while unistd::read(STDIN_FILENO, &mut c).is_ok() 
         && c[0] != 113{
@@ -12,10 +12,28 @@ fn main() {
     return;
 }
 
-fn enable_raw_mode() {
-    let mut termios = Termios::from_fd(STDIN_FILENO).unwrap();
+pub struct RawTerminal {
+    preview_terminal: Termios,
+}
 
-    termios.c_lflag &= !(ECHO);
+impl RawTerminal {
+    fn enable_raw_mode() -> RawTerminal {
+        let mut termios = Termios::from_fd(STDIN_FILENO).unwrap();
+        let preview_terminal = termios;
+        termios.c_lflag &= !(ECHO);
+        tcsetattr(STDIN_FILENO, TCSAFLUSH, &termios).unwrap();
+        RawTerminal {
+            preview_terminal,
+        }
+    }
 
-    tcsetattr(STDIN_FILENO, TCSANOW, &termios).unwrap();
+    fn disable_raw_mode(&self) {
+       tcsetattr(STDIN_FILENO, TCSAFLUSH, &self.preview_terminal).unwrap();
+    }
+}
+
+impl Drop for RawTerminal {
+    fn drop(&mut self) {
+        self.disable_raw_mode();
+    }
 }
