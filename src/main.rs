94d251efe8b190b5a98bcd5b66ef459f845082ee
riko-teki <boiledshrimps0109@gmail.com>
2022-02-02 14:53:40 +0900

    step 4 and step 5

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
 use libc::STDIN_FILENO;
 use nix::unistd;
+use termios::*;
 
 fn main() {
+    enable_raw_mode();
     let mut c: [u8;1] = [0];
-    while unistd::read(STDIN_FILENO, &mut c).is_ok() {
+    while unistd::read(STDIN_FILENO, &mut c).is_ok() 
+        && c[0] != 113{
         
     }
     return;
 }
 
+fn enable_raw_mode() {
+    let mut termios = Termios::from_fd(STDIN_FILENO).unwrap();
+
+    termios.c_lflag &= !(ECHO);
+
+    tcsetattr(STDIN_FILENO, TCSANOW, &termios).unwrap();
+}
